0de2a581c064f27875818ed5031da27cf44a3181
Henrik Kiepe <drgoodcat77@googlemail.com>
2023-05-03 16:32:46 +0000

    fixed imports

--- src/layouts/TutorialsLayout.jsx
+++ src/layouts/TutorialsLayout.jsx
@@ -1,11 +1,6 @@
 // rrd imports
 import { Outlet } from "react-router-dom";
 
-// components
-import Header from "../components/headers/light";
-import Footer from "../components/footers/FiveColumnWithInputForm";
-import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-
 const TutorialsLayout = () => {
   return <Outlet />;
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,22 +4,22 @@ import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import illustration from "images/signup-illustration.svg";
-import logo from "images/logo.svg";
-import googleIconImageSrc from "images/google-icon.png";
-import twitterIconImageSrc from "images/twitter-icon.png";
+// import logo from "images/logo.svg";
+// import googleIconImageSrc from "images/google-icon.png";
+// import twitterIconImageSrc from "images/twitter-icon.png";
 import { ReactComponent as SignUpIcon } from "feather-icons/dist/icons/user-plus.svg";
 
 const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
 const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
 const LogoLink = tw.a``;
-const LogoImage = tw.img`h-12 mx-auto`;
+// const LogoImage = tw.img`h-12 mx-auto`;
 const MainContent = tw.div`mt-12 flex flex-col items-center`;
 const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
 const FormContainer = tw.div`w-full flex-1 mt-8`;
 
-const SocialButtonsContainer = tw.div`flex flex-col items-center`;
-const SocialButton = styled.a`
+// const SocialButtonsContainer = tw.div`flex flex-col items-center`;
+{/*const SocialButton = styled.a`
   ${tw`w-full max-w-xs font-semibold rounded-lg py-3 border text-gray-900 bg-gray-100 hocus:bg-gray-200 hocus:border-gray-400 flex items-center justify-center transition-all duration-300 focus:outline-none focus:shadow-outline text-sm mt-5 first:mt-0`}
   .iconContainer {
     ${tw`bg-white p-2 rounded-full`}
@@ -30,7 +30,7 @@ const SocialButton = styled.a`
   .text {
     ${tw`ml-4`}
   }
-`;
+`;*/}
 
 const DividerTextContainer = tw.div`my-12 border-b text-center relative`;
 const DividerText = tw.div`leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform -translate-y-1/2 absolute inset-x-0 top-1/2 bg-transparent`;
